eae5b5c517afce12a01a498b722470d6f0a70594
Mark-O-S <>
2023-06-01 20:41:13 +0000

    Fix admin adding personal training object

--- gym/models.py
+++ gym/models.py
@@ -6,10 +6,9 @@ class PersonalTraining(models.Model):
     Defines personal training object
     """
 
-
     available = models.BooleanField(null=False, blank=False, default=True)
     user_id = models.CharField(max_length=50, null=True, blank=True)
     date_time = models.DateTimeField(null=False, blank=False)
 
     def __str__(self):
-        return self.date_time
+        return str(self.date_time)
